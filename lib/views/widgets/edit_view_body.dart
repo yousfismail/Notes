72d1769c02519eb3aec4c8be05b3a777f43cd419
yousfismail <yousfismail1200@gmail.com>
2023-05-01 00:34:14 +0300

    return GestureDetector

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -4,3 +4,9 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:myapp/views/widgets/custom_app_bar.dart';
 import 'package:myapp/views/widgets/custom_text_field.dart';
 
+class EditNoteViewBody extends StatelessWidget {
+  const EditNoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding
