c8d16d5162e7b0fffb7a0ca913186f23d6ad8019
yousfismail <yousfismail1200@gmail.com>
2023-05-01 00:30:41 +0300

    Add Widget (class EditNoteView)

---
+++ lib/views/edit_note_view.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:myapp/views/widgets/edit_view_body.dart';
+
+class EditNoteView extends StatelessWidget
