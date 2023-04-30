abfedd0fd654158b122b4eb1612e245c22df0ded
yousfismail <yousfismail1200@gmail.com>
2023-04-30 22:35:38 +0300

    create folder (views) in lib

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,7 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:myapp/views/widgets/add_note_bottom_sheet.dart';
+//Change theme and creat notes view 
