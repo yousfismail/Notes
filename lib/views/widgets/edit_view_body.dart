8be5df4e45aab21c2f1e916608b5134fe238012b
yousfismail <yousfismail1200@gmail.com>
2023-05-01 00:42:44 +0300

    Create edit note view

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -11,7 +11,7 @@ class EditNoteViewBody extends StatelessWidget  {
   Widget build(BuildContext context) {
     return Padding (
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
+      child: Column (
         children: const [
           SizedBox(height: 50,),
           CustomAppBar(title: 'Edit Note', icon: Icons.check,),
