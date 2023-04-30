cf22a74ddb9467d92f39909be0f8ac7926a62b77
yousfismail <yousfismail1200@gmail.com>
2023-04-30 22:53:02 +0300

    Add style widget and TextStyle

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -1,2 +1,23 @@
  import 'package:flutter/material.dart';
  import 'custom_icon.dart';
+
+ class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key, required this.title, required this.icon});
+
+final String title;
+final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children:  [
+        Text(title , style: const TextStyle(fontSize: 28,
+         ),
+        ),
+        Spacer(),
+        CustomSearchIcon(
+          icon: icon,
+        ),
+      ],
+    );
+  }
+}
