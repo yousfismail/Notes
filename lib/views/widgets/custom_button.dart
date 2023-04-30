3cb9ba85c574d36fa4ee6334c8e2d525025574ed
yousfismail <yousfismail1200@gmail.com>
2023-05-01 00:27:51 +0300

    add code (CustomButton) in file

---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
