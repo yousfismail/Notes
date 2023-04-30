1b98f6a58367046b3b5c671c73abf53ec7a2a43a
yousfismail <yousfismail1200@gmail.com>
2023-04-30 22:30:10 +0300

    create class

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,16 @@ void main() async {
   await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark ,
+       fontFamily: 'Poppins'),
+      home: const NotesView(), 
+    );
+  }
+}
