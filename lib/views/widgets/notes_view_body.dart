b7f8e1964c14cf858ce8482acc2b7c4b29238c0d
yousfismail <yousfismail1200@gmail.com>
2023-04-30 22:39:57 +0300

    import material.dart

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -3,3 +3,25 @@ import 'package:myapp/views/widgets/custom_app_bar.dart';
 
 import 'custom_note_item.dart';
 import 'notes_list_view.dart';
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const [ 
+          SizedBox(
+            height: 50,
+          ),
+          
+           CustomAppBar(title: 'Notes', icon: Icons.search,),
+           Expanded(child: NotesListView()),
+           ],
+    
+      ),
+    );
+    
+  }
+}
