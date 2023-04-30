7f4e32185b0a506a048096a03499062aed2d1656
yousfismail <yousfismail1200@gmail.com>
2023-04-30 22:58:36 +0300

    Add width = 46 in CustomSearchIcon

--- lib/views/widgets/custom_icon.dart
+++ lib/views/widgets/custom_icon.dart
@@ -7,7 +7,7 @@ final IconData icon;
   Widget build(BuildContext context) {
     return Container(
       height: 46,
-      width: 46,
+      width: 46 ,
       decoration: BoxDecoration(
         color: Colors.white.withOpacity(.05),
         borderRadius: BorderRadius.circular(16),
