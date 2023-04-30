535f25ed21f4be231779dda614fbdaa8456b2f7b
yousfismail <yousfismail1200@gmail.com>
2023-04-30 22:29:17 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:myapp/views/notes_view.dart';
+
+import 'constants.dart';
+
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(kNotesBox);
+  runApp(const NotesApp());
+}
