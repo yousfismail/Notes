0f20ff06554516cc81a8c70bf753ac5514b56bc4
yousfismail <yousfismail1200@gmail.com>
2023-05-01 00:41:51 +0300

    Link between the first file and the second file

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -4,9 +4,23 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:myapp/views/widgets/custom_app_bar.dart';
 import 'package:myapp/views/widgets/custom_text_field.dart';
 
-class EditNoteViewBody extends StatelessWidget {
+class EditNoteViewBody extends StatelessWidget  {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const [
+          SizedBox(height: 50,),
+          CustomAppBar(title: 'Edit Note', icon: Icons.check,),
+          SizedBox(height: 50,),
+          CustomTextField(hint: 'Title'),
+          SizedBox(height: 16,),
+          CustomTextField(hint: 'Content' , maxLines: 5,),
+        ],
+      ),
+    );
+  }
+}
