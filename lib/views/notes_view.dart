4f61640c8db73aa69e48272bacb4a75b32dbd513
yousfismail <yousfismail1200@gmail.com>
2023-04-30 22:37:01 +0300

    view notes

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -6,3 +6,20 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:myapp/views/widgets/add_note_bottom_sheet.dart';
 //Change theme and creat notes view 
 import 'widgets/notes_view_body.dart';
+class NotesView extends StatelessWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return   Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        showModalBottomSheet( shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)) , context: context, builder: (context){
+          return const AddNoteBottomSheet();
+        });
+         
+      } , child: const Icon(Icons.add),),
+    body: const NotesViewBody(),
+    );
+  }
+}
+
