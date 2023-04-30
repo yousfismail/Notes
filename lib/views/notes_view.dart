13e3d5aedf3ce7befd84d7b89f67ee573455cec4
yousfismail <yousfismail1200@gmail.com>
2023-04-30 22:36:24 +0300

    Change theme and creat notes view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -5,3 +5,4 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:myapp/views/widgets/add_note_bottom_sheet.dart';
 //Change theme and creat notes view 
+import 'widgets/notes_view_body.dart';
