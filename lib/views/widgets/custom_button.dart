d8bcc6b31d3fdeed6fffbdf5c73e73ae1e6188b0
yousfismail <yousfismail1200@gmail.com>
2023-05-01 00:28:15 +0300

    import constants.dart

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
 import '../../constants.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({super.key});
+    @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(color: kPrimaryColor , borderRadius: BorderRadius.circular(8)),
+      child: const Center(
+        child: Text('Add',style: TextStyle(color: Colors.black , fontWeight: FontWeight.bold , fontSize: 20),),
+      ),
+
+    );
+  }
+}
