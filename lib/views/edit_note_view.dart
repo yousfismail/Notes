2f3863bc0302d9ed37c2b958536665e8900cae54
yousfismail <yousfismail1200@gmail.com>
2023-05-01 00:31:15 +0300

    add Widget body(EditNoteViewBody)

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -3,4 +3,11 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:myapp/views/widgets/edit_view_body.dart';
 
-class EditNoteView extends StatelessWidget
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
