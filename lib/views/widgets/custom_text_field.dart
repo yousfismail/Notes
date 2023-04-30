f62eec53736684ff3f189e02b4b60d272cc9e67a
yousfismail <yousfismail1200@gmail.com>
2023-04-30 23:13:37 +0300

    Call CustomTextField in (AddNoteBottomSheet)

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -1,3 +1,29 @@
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hint,  this.maxLines = 1});
+
+  final String hint;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      maxLines: maxLines,
+      decoration: InputDecoration( hintText: hint , hintStyle: const TextStyle(color: kPrimaryColor) , border: buildBorder(),
+      enabledBorder: buildBorder(),
+      focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([Color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: Color ?? Colors.white)
+    );
+  }
+}
