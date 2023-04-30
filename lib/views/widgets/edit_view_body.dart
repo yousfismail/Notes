6e8dfe644761172080f586812f29a93b5b8d4916
yousfismail <yousfismail1200@gmail.com>
2023-05-01 00:33:51 +0300

    Link between the first file and the second file

---
+++ lib/views/widgets/edit_view_body.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:myapp/views/widgets/custom_app_bar.dart';
+import 'package:myapp/views/widgets/custom_text_field.dart';
+
